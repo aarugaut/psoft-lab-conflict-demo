ee6e92880fe17f5bb18e03d327e8a4e820607843
aarugaut <aaryangaut@gmail.com>
2026-01-29 22:03:26 -0500

    Aaryan changes #2

--- main.java
+++ main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Version 2 - my changes");
+        System.out.println("Version 2 - Aaryans changes");
         System.out.println("Version 3 - Eli changes");
     }
 }
